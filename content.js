fd692708bf33a21418050db10e6076f186810d88
Kalen Jordan <kalen.jordan@kalenjo6mk6mx9j.lan>
2023-07-17 11:51:15 -0500

    Working

--- content.js
+++ content.js
@@ -1,38 +1,47 @@
 chrome.runtime.sendMessage({method: "getSetting", key: "mySetting"}, function(response) {
     let colorMapping = JSON.parse(response);    
-    console.log('Color-Coded Shopify Orders Color Mapping: ');
-    console.log(colorMapping); // This will log the value of "mySetting"
+    console.log('Color-Coded Shopify Orders Color Mapping');
+    // console.log(colorMapping); // This will log the value of "mySetting"
 
     // applyColorToOrderRows(colorMapping);
-
       
-    let intervalId = setInterval(function(colorMapping) {
+    let intervalId = setInterval(function() {
         let rows = document.querySelectorAll('[class*="Polaris-IndexTable__TableRow"]');
         if (rows.length) {
-            console.log("Order Rows HTML Are Loaded:");
-            console.log(rows);
-            applyColorToOrderRows(colorMapping, rows);
+            console.log("Order Rows HTML Are Loaded");
             clearInterval(intervalId);  // Stop checking.
+            applyColorToOrderRows(colorMapping, rows);
         }
     
     }, 1000);
 
 });
 
-function checkForSelector() 
+function applyColorToOrderRows(colorMapping, rows) 
 {
-    let element = document.querySelector(cssSelector);
-    if (element) {
-      console.log('Element found!');
-      clearInterval(intervalId);  // Stop checking.
-      // Here you can perform additional actions with the element.
-    } else {
-      console.log('Element not found.');
+    //let row = rows[0];
+    //applyColorToOneOrderRow(colorMapping, row);
+    console.log("rows");
+    console.log(rows);
+    for (let row of rows) {
+        applyColorToOneOrderRow(colorMapping, row);
     }
 }
 
-function applyColorToOrderRows(colorMapping, rows) 
+function applyColorToOneOrderRow(colorMapping, row)
 {
-    // loop over rows
-    // row.querySelectorAll('[class*="Polaris-Tag__TagText"]');
+    let tags = row.querySelectorAll('[class*="Polaris-Tag__TagText"]');
+    for (let tag of tags) {
+        console.log("tag: ");
+        console.log(tag);
+        console.log(tag.textContent);        
+        let tagText = tag.textContent;
+
+        // console.log(colorMapping);
+        console.log("color:");
+        console.log(colorMapping[tagText]);
+        if (colorMapping[tagText]) {
+            row.style.backgroundColor = colorMapping[tagText];
+        }
+    }
 }
